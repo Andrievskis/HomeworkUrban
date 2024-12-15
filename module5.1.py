866e682dd727bd93ea801398b85758a0f823c3d9
Anastasia <nnefedova1@icloud.com>
2024-12-15 00:05:53 -0800

    Add files via upload

---
+++ module5.1.py
@@ -0,0 +1,38 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        lower_floor = 0
+        while new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует.')
+            break
+        else:
+            for lower_floor in range(new_floor):
+                print(lower_floor + 1)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
+
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.number_of_floors = floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print('Такого этажа не существует.')
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
