958a29e286ee5806cdd0685d81fb44e73cec43b1
Anastasia <nnefedova1@icloud.com>
2024-12-14 23:46:39 -0800

    Add files via upload

--- Module_1/module_1_6.py
+++ Module_1/module_1_6.py
@@ -1,4 +1,20 @@
-number_of_completed_homework_assignments = 12
-number_of_hours_spent = 1.5
-course_name = "Python"
-print('Курс: ', (course_name), "всего задач:", (number_of_completed_homework_assignments ), ", затрачено часов:", (number_of_hours_spent), ", среднее время выполнения:", (number_of_hours_spent/number_of_completed_homework_assignments), 'часа.')
+from lesson_07 import list_
+
+my_dict = {"Anna": 1998, "Tina": 2000, "Pit": 1999}
+print("Dict:", my_dict)
+print(my_dict["Tina"])
+print("Not existing value:", my_dict.get("Nina"))
+my_dict.update({"Luna": 2002,
+                "Tim": 1997})
+d = my_dict.pop("Anna")
+print("Deleted value:", d)
+print("Modified dictionary:", my_dict)
+
+my_set = {1, 1, 2, 3.34, 3.34, 4, "a", "a", "c", "cat", "cat"}
+print("Set:", my_set)
+print(my_set.add(8))
+print(my_set.add("Y"))
+print(my_set.discard("cat"))
+print("Modified set:", my_set)
+
+
