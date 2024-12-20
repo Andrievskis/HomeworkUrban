db7ce78dce0622c4ee1c9e53c7fb20f46d546a79
Anastasia <nnefedova1@icloud.com>
2024-12-19 23:50:51 -0800

    Add files via upload

--- Module_6/module_6.2.py
+++ Module_6/module_6.2.py
@@ -1,39 +1,41 @@
 class Vehicle:
-    def __init__(self, owner, _model, _color, _engine_power):
+    def __init__(self, owner, model, engine_power, color):
         self.owner = owner
-        self._model = _model
-        self._engine_power = _engine_power
-        self._color = _color
-        self._COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
+
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
     def get_model(self):
-        return f'Модель: {self._model}'
+        return f'Модель: {self.__model}'
 
     def get_horsepower(self):
-        return f'Мощность двигателя: {self._engine_power}'
+        return f'Мощность двигателя: {self.__engine_power}'
 
     def get_color(self):
-        return f'Цвет: {self._color}'
+        return f'Цвет: {self.__color}'
 
     def print_info(self):
-        print(self.get_model())
-        print(self.get_horsepower())
-        print(self.get_color())
+        print(self.get_model(),
+              self.get_horsepower(),
+              self.get_color(),
+              sep='\n'
+              )
         print(f'Владелец: {self.owner}')
 
     def set_color(self, new_color):
-        self.new_color = new_color
-        if new_color.lower() in self._COLOR_VARIANTS:
-            self._color = self.new_color
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
         else:
-            print(f'Нельзя сменить цвет на {self.new_color}')
+            print(f'Нельзя сменить цвет на {new_color}')
 
 
 class Sedan(Vehicle):
-    _PASSENGERS_LIMIT = 5
+    __PASSENGERS_LIMIT = 5
 
 
-vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
 vehicle1.print_info()
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
